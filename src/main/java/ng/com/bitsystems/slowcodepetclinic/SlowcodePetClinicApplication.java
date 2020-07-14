b6b7084ef06991edbec0d6e545e4875742afee75
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-14 22:18:43 +0100

    created the project and updated git-ignore

---
+++ src/main/java/ng/com/bitsystems/slowcodepetclinic/SlowcodePetClinicApplication.java
@@ -0,0 +1,13 @@
+package ng.com.bitsystems.slowcodepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowcodePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowcodePetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ng/com/bitsystems/slowcodepetclinic/SlowcodePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.com.bitsystems.slowcodepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowcodePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
