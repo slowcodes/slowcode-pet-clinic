9ecbcce17c64b14583f18d44e293a4332616569e
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-14 22:24:07 +0100

    created the project and updated git-ignore

--- src/main/java/ng/com/bitsystems/slowcodepetclinic/SlowcodePetClinicApplication.java
+++ src/main/java/ng/com/bitsystems/slowcodepetclinic/SlowcodePetClinicApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SlowcodePetClinicApplication {
 
     public static void main(String[] args) {
+        //boiler plate
         SpringApplication.run(SlowcodePetClinicApplication.class, args);
     }
 
