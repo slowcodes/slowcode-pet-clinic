cb1aeef10f737ad7409984e35b7f88bb17182079
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-17 00:51:19 +0100

    Update the OwnerController to inject the OwnerServiceMap into the controller and create a thymeleaf template to display all owners

--- pet-clinic-web/src/main/java/ng/com/bitsystems/slowcodepetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ng/com/bitsystems/slowcodepetclinic/controllers/OwnerController.java
@@ -1,5 +1,6 @@
 package ng.com.bitsystems.slowcodepetclinic.controllers;
 
+import ng.com.bitsystems.slowcodepetclinic.services.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerServiceMap ownerServiceMap;
+
+    public OwnerController(OwnerServiceMap ownerServiceMap) {
+        this.ownerServiceMap = ownerServiceMap;
+    }
+
     @RequestMapping({"", "/","/index.html", "/index"})
     public String listOwners(Model model){
+        model.addAttribute("owners",ownerServiceMap.findAll());
 
         return "owners/index";
     }
