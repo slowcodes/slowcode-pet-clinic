bca257d080bce6ba69a4583ed82423c56fdc19ac
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-18 17:07:15 +0100

    Create Pet Type Map Service Implementation. Closes #29.

---
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ng.com.bitsystems.slowcodepetclinic.services;
+
+import ng.com.bitsystems.slowcodepetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package ng.com.bitsystems.slowcodepetclinic.services.map;
+
+import ng.com.bitsystems.slowcodepetclinic.model.PetType;
+import ng.com.bitsystems.slowcodepetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType add(PetType object) {
+        return super.add(object);
+    }
+
+    @Override
+    public PetType findByID(Long id) {
+        return super.findById(id);
+    }
+}
