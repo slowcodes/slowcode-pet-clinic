31ec8672660c4499b772dcb82dcee6c76347d14b
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-18 16:25:03 +0100

    Create Pet Type, Pet and Visit Entities. Closes #26.

---
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Visit.java
@@ -0,0 +1,12 @@
+package ng.com.bitsystems.slowcodepetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity{
+
+    private LocalDate localDate;
+    private String description;
+    private Pet pet;
+
+
+}
