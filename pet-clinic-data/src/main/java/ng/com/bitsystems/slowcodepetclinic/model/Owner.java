4be8b364b2256f2bffaa85ed30a4a73abf48c4fc
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-18 16:37:01 +0100

    Add Contact into Properties to owners. Closes #28.

--- pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Owner.java
@@ -3,7 +3,34 @@ package ng.com.bitsystems.slowcodepetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String tel;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
 
     public Set<Pet> getPets() {
         return pets;
