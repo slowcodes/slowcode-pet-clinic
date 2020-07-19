d81286a0cf5d3fe53b652956bb85494f52c0d003
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-19 02:48:36 +0100

    Create specialty service Map implementation. Closes #31.

---
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package ng.com.bitsystems.slowcodepetclinic.services;
+
+import ng.com.bitsystems.slowcodepetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
