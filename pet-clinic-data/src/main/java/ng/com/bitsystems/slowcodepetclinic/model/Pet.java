8ef908c03b55e365709067cd5a89826b712e1ed9
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-23 14:46:23 +0100

    Implement base entity (JPA). Closes #35.

--- pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Pet.java
@@ -1,11 +1,24 @@
 package ng.com.bitsystems.slowcodepetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
@@ -15,8 +28,6 @@ public class Pet extends BaseEntity {
     public void setName(String name) {
         this.name = name;
     }
-
-    private String name;
 
     public PetType getPetType() {
         return petType;
