bd581c8e59488a4a08f4cc763e9e1dd9057cff40
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-23 15:09:04 +0100

    convert vets to JPA entities. Closes #37.

--- pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Speciality.java
@@ -1,6 +1,14 @@
 package ng.com.bitsystems.slowcodepetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ng/com/bitsystems/slowcodepetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package ng.com.bitsystems.slowcodepetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns= @JoinColumn( name= "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {
